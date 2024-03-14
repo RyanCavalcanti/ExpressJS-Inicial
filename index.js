a39ea7e349b82f96e0e7a3d9d07bf8e22160faa2
Ryan Cavalcanti <contatodevryan@gmail.com>
2024-03-13 23:49:51 -0300

    Adding get and post methods

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/alunos', (req, res) => {
+    console.log(req.query)
+    console.log("Entrou no Alunos")
+    res.status(200).send('Deu certo!')
+})
+
+app.get('/alunos/:id/:outro', (req, res) => {
+    console.log(req.params.id)
+    console.log(req.params.outro)
+    console.log("Entrou no Alunos")
+    res.status(200).send('Deu certo!')
+})
+
+app.post('/alunos', (req, res) => {
+    console.log(req.body)
+    console.log("Entrou no Alunos")
+    res.status(200).send(req.body)
+})
+
 app.listen(3000, () => {
     console.log("Servidor iniciado")
 })
