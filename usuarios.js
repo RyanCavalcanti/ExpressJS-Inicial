03df740edf8a4792606c9f11e37bae2c6de127af
Ryan Cavalcanti <contatodevryan@gmail.com>
2024-03-14 14:27:11 -0300

    Adding id to crud

--- usuarios.js
+++ usuarios.js
@@ -3,22 +3,26 @@ const app = express();
 
 app.use(express.json());
 
+const users = []
+
 app.listen(3000, () => {
     console.log("Servidor iniciado")
 })
 
 app.get('/usuarios', (req, res) => {
     console.log('API get usuários')
-    res.status(200).send("Todos os usuários")
+    res.status(200).send({users: users})
 })
 
 app.get('/usuarios/:id', (req, res) => {
-    console.log('API get por id')
-    res.status(200).send(req.params.id)
+    const alunoID = req.params.id;
+    console.log('API get por id');
+    res.status(200).send(users.find(x => x.id == alunoID));
 })
 
 app.post('/usuarios', (req, res) => {
     console.log('API post usuário')
+    users.push(req.body)
     res.status(200).send(req.body)
 })
 
